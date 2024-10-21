35f39de81101b8d72b1a8b3e05b7a50482b56bd3
RubenRodriguez897 <rben89718@gmail.com>
2024-10-21 09:32:32 -0600

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,12 @@
+<?php
+/* CBTIS 89 
+   Programa contiene mis datos personales
+   Ruben Rodríguez Flores
+   3ºA Programaciòn Matutino
+   */
+echo "Mi nombre es Ruben Rodriguez Flores";
+echo "<br>","Estudio en el CBTIS 89";
+echo "<br>", "En tercer semestre de la carrera de programación A Matutino";
+echo "<br>","Vivo en la ciudad de Durango,Dgo";
+
+?>
